5f71c50936e1292b89732c47a91654c966c13199
Mariano Carrazana <marianocarrazana@gmail.com>
2023-06-10 18:44:22 -0300

    Add buttons and logs functions

--- src/dockery/gui.py
+++ src/dockery/gui.py
@@ -1,6 +1,18 @@
+from rich.text import TextType
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, MarkdownViewer, Markdown, Static, Button
-from textual.containers import ScrollableContainer, VerticalScroll, Container as Group
+from textual.widgets import (
+    Footer,
+    Header,
+    Static,
+    Button,
+    ContentSwitcher,
+)
+from textual.containers import (
+    VerticalScroll,
+    Horizontal,
+    Vertical,
+    Container as Group,
+)
 from docker import DockerClient
 from docker.models.containers import Container
 
@@ -8,26 +20,32 @@ from docker.models.containers import Container
 class AppGUI(App):
     CSS_PATH = "style.css"
     BINDINGS = [
+        ("h", "home", "Home"),
         ("d", "toggle_dark", "Toggle dark mode"),
         ("q", "quit", "Quit"),
     ]
-    TITLE = "Satori CLI"
+    TITLE = "DOCKERY"
 
     def __init__(self, docker: DockerClient, **kargs):
         self.docker = docker
         super().__init__(**kargs)
 
     def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
         yield Header(show_clock=True)
         yield Footer()
-        yield VerticalScroll(id="container-list")
+        with ContentSwitcher(initial="container-list"):
+            yield VerticalScroll(id="container-list")
+            with VerticalScroll(id="container-logs"):
+                yield Static("", id="logs")
 
     def on_mount(self) -> None:
         cl = self.query_one("#container-list")
         for c in self.docker.containers.list(all=True):
             cw = ContainerWidget(c)  # type: ignore
             cl.mount(cw)
+
+    def action_home(self) -> None:
+        self.query_one(ContentSwitcher).current = "container-list"
 
 
 class ContainerWidget(Static):
@@ -36,6 +54,82 @@ class ContainerWidget(Static):
         super().__init__(**kargs)
 
     def compose(self) -> ComposeResult:
-        """Create child widgets of a stopwatch."""
-        yield Group(Static(self.container.name or ""), Static(self.container.short_id))
-        yield Group(Static(self.container.status))
+        yield Group(
+            Static(self.container.name or ""),
+            Static(self.container.short_id),
+            classes="container-info",
+        )
+        yield Vertical(Static(self.container.status), classes="status-text")
+        yield Group(StatusButtons(self.container))
+
+
+class StatusButtons(Static):
+    def __init__(self, container: Container, **kargs):
+        self.container = container
+        self.button = StartStopButton(container)
+        super().__init__(**kargs)
+
+    def compose(self) -> ComposeResult:
+        yield Horizontal(
+            self.button,
+            RestartButton(self.container),
+            LogsButton(self.container),
+            # Button("Details"),
+            classes="buttons-container",
+        )
+
+
+class StartStopButton(Button):
+    def __init__(self, container: Container, **kargs):
+        self.running = container.status == "running"
+        self.container = container
+        super().__init__(**kargs)
+
+    def on_click(self) -> None:
+        self.running = not self.running
+        if self.running:
+            self.container.start()
+        else:
+            self.container.stop()
+        self.on_mount()
+
+    def on_mount(self) -> None:
+        self.variant = "error" if self.running else "success"
+        self.text = "Stop" if self.running else "Start"
+
+    def render(self) -> TextType:
+        return self.text
+
+
+class RestartButton(Button):
+    def __init__(self, container: Container, **kargs):
+        self.container = container
+        super().__init__(**kargs)
+
+    def on_click(self) -> None:
+        self.container.restart()
+
+    def on_mount(self) -> None:
+        self.variant = "warning"
+
+    def render(self) -> TextType:
+        return "Restart"
+
+
+class LogsButton(Button):
+    def __init__(self, container: Container, **kargs):
+        self.container = container
+        super().__init__(**kargs)
+
+    def on_click(self) -> None:
+        logs: bytes = self.container.logs(tail=20)
+        cl = self.app.query_one("#logs")
+        lw = Static(logs.decode())
+        cl.mount(lw)
+        self.app.query_one(ContentSwitcher).current = "container-logs"
+
+    def on_mount(self) -> None:
+        self.variant = "primary"
+
+    def render(self) -> TextType:
+        return "Logs"

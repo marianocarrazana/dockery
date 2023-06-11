54ffdb915bff590fb9cadeed2ec645c244f9af93
Mariano Carrazana <marianocarrazana@gmail.com>
2023-06-11 15:28:59 -0300

    Add server, ssh and api-version cli params Add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+## 0.1.1
+
+* Add `--server`, `--ssh` and `--api-version` cli parameters
+
+## 0.1.0
+
+* First public release
--- src/dockery/dockery.py
+++ src/dockery/dockery.py
@@ -4,10 +4,21 @@ from .gui import AppGUI
 
 
 @click.command()
-@click.option("--port", default=0, help="port")
-@click.option("--host", default=0, help="host")
-def main(port, host):
-    client = docker.from_env()
+@click.option(
+    "--server",
+    default=None,
+    help="Docker server url, for example: unix:///var/run/docker.sock",
+)
+@click.option("--ssh", is_flag=True)
+@click.option("--api-version", default="1.35", help="Docker API version")
+def main(server, ssh, api_version):
+    print(server, ssh, api_version)
+    if server is None:
+        client = docker.from_env()
+    else:
+        client = docker.DockerClient(
+            base_url=server, version=api_version, use_ssh_client=ssh
+        )
     gui = AppGUI(client)
     gui.run()
 
